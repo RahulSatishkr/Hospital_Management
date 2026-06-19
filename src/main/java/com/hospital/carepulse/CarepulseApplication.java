660e3e968a09a195d2d3d7be86cff29140217d96
RahulSatishkr <rahul.satishkr@gmail.com>
2026-06-19 09:11:20 -0500

    Initial commit: Secure Spring Boot skeleton

---
+++ src/main/java/com/hospital/carepulse/CarepulseApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.carepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarepulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarepulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/carepulse/CarepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.carepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarepulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
